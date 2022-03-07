154f6e653e9ec31d26b28e05dc8774b0847207e9
Oussama Louati <92301144+OssamaLouati@users.noreply.github.com>
2022-03-08 00:33:25 +0100

    update

--- scipt.js
+++ scipt.js
@@ -1,6 +1,5 @@
 const video = document.getElementById('video')
 
-
 Promise.all([
     faceapi.nets.tinyFaceDetector.loadFromUri('/Emotion_Recognition-AI-Javascript/models'),
     faceapi.nets.faceLandmark68Net.loadFromUri('/Emotion_Recognition-AI-Javascript/models'),
@@ -16,17 +15,25 @@ function startVideo(){
     )
 }
 
+function stratvid(){
+  navigator.getUsermEdia(
+    {video!: {} },
+  stream => video.srcObject = stream,
+err => console.log(err)
+  )
+}
+
 video.addEventListener('playing', () => {
-    const canvas = faceapi.createCanvasFromMedia(video)
-    document.body.append(canvas)
-    const displaySize = { width: video.width, height: video.height }
-    faceapi.matchDimensions(canvas, displaySize)
-    setInterval(async () => {
-      const detections = await faceapi.detectAllFaces(video, new faceapi.TinyFaceDetectorOptions()).withFaceLandmarks().withFaceExpressions()
-      const resizedDetections = faceapi.resizeResults(detections, displaySize)
-      canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height)
-      faceapi.draw.drawDetections(canvas, resizedDetections)
-     // faceapi.draw.drawFaceLandmarks(canvas, resizedDetections)
-      faceapi.draw.drawFaceExpressions(canvas, resizedDetections)
-    }, 100)
-  })
+  const canvas = faceapi.createCanvasFromMedia(video)
+  document.body.append(canvas)
+  const displaySize = { width: video.width, height: video.height }
+  faceapi.matchDimensions(canvas, displaySize)
+  setInterval(async () => {
+    const detections = await faceapi.detectAllFaces(video, new faceapi.TinyFaceDetectorOptions()).withFaceLandmarks().withFaceExpressions()
+    const resizedDetections = faceapi.resizeResults(detections, displaySize)
+    canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height)
+    faceapi.draw.drawDetections(canvas, resizedDetections)
+    //faceapi.draw.drawFaceLandmarks(canvas, resizedDetections)
+    faceapi.draw.drawFaceExpressions(canvas, resizedDetections)
+  }, 100)
+})
