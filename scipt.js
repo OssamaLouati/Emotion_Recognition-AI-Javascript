ab4803827ec6c1eaceaa8a5f7666247437dcf6ac
Oussama Louati <louati.oussama@ine.inpt.ma>
2022-12-21 15:50:09 +0100

    finish

--- scipt.js
+++ scipt.js
@@ -25,7 +25,7 @@ video.addEventListener('play', () => {
     const resizedDetections = faceapi.resizeResults(detections, displaySize)
     canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height)
     faceapi.draw.drawDetections(canvas, resizedDetections)
-    //faceapi.draw.drawFaceLandmarks(canvas, resizedDetections)
+    faceapi.draw.drawFaceLandmarks(canvas, resizedDetections)
     faceapi.draw.drawFaceExpressions(canvas, resizedDetections)
   }, 100)
 })
